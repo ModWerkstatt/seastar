8af5792e4046e3acd6af26c9a81fcdce41bd7e97
Florian Thiel <flth@flth.de>
2024-12-19 22:49:26 +0100

    verschiebe Ordner

---
+++ strings.lua
@@ -0,0 +1,27 @@
+function data()
+return {
+		en = {
+		
+			["mod_name"] = "Dornier Seastar",
+			["mod_desc"] = "",
+			
+			["seastar_menu"] = "Dornier Seastar",
+			["seastar_1_name"] = "Dornier Seastar",
+			
+            ["seastar_desc"] = "",
+			
+		},	
+		de = {
+		
+			["mod_name"] = "Dornier Seastar",
+			["mod_desc"] = "",
+			
+			["seastar_menu"] = "Dornier Seastar",
+			["seastar_1_name"] = "Dornier Seastar",
+			
+            ["seastar_desc"] = "",
+			
+		},
+		
+	}
+end
