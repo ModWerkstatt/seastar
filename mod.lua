b78c2595a7b03a1d55238c851279330bae3a54dc
Florian Thiel <flth@flth.de>
2024-12-20 22:23:17 +0100

    Formatierung

--- mod.lua
+++ mod.lua
@@ -1,24 +1,19 @@
 function data()
 return {
-info = {
-  minorVersion = 0,
-  severityAdd = "NONE",
-  severityRemove = "WARNING",
-  name = _("mod_name"),
-  description = _("mod_desc"),
-  authors = {
-  
-        {
-            name = "Bandion",
-            role = "CREATOR",
-        },
-		
-			},
-			
-  tags = {"europe", "plane", },
-  		visible = true,
-
-		},
-
+    info = {
+      minorVersion = 0,
+      severityAdd = "NONE",
+      severityRemove = "WARNING",
+      name = _("mod_name"),
+      description = _("mod_desc"),
+      authors = {
+            {
+                name = "Bandion",
+                role = "CREATOR",
+            },
+      },
+      tags = {"europe", "plane", },
+      visible = true,
+   },
 }
 end
