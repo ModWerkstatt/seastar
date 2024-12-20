9b363de8931e58225f130045f6f86e4cac131814
Florian Thiel <flth@flth.de>
2024-12-20 22:23:10 +0100

    Texte angepasst

--- strings.lua
+++ strings.lua
@@ -1,27 +1,31 @@
 function data()
 return {
-		en = {
-		
-			["mod_name"] = "Dornier Seastar",
-			["mod_desc"] = "",
-			
-			["seastar_menu"] = "Dornier Seastar",
-			["seastar_1_name"] = "Dornier Seastar",
-			
-            ["seastar_desc"] = "",
-			
-		},	
 		de = {
-		
+
 			["mod_name"] = "Dornier Seastar",
-			["mod_desc"] = "",
-			
+			["mod_desc"] = "Die Dornier Seastar (\"Seestern\") ist ein zweimotoriges Amphibienflugzeug von Dornier Seawings. Das Flugzeug verfügt über Kurzstart- und -landeeigenschaften. In seiner Fluggastversion ist es für zwei Piloten und 12 Passagiere (oder einen Piloten und 9 Passagiere) ausgelegt. Wesentliche Grundprinzipien seiner Konzeption wurden von der bereits in den 1920er Jahren erfolgreichen Dornier-Wal-Baureihe übernommen und fortentwickelt; so findet sich bei dieser Maschine die Hochdeckerbauweise mit zusätzlichen untenliegenden Stummelflügeln ebenso wieder wie die Tandemanordnung der beiden Motoren. ",
+
 			["seastar_menu"] = "Dornier Seastar",
 			["seastar_1_name"] = "Dornier Seastar",
-			
-            ["seastar_desc"] = "",
-			
+            ["Seastar Asset Set"] = "Seastar Asset Set",
+            ["Statische Seastar Flugzeuge."] = "Statische Seastar Flugzeuge.",
+
+            ["seastar_desc"] = "Die Dornier Seastar (\"Seestern\") ist ein zweimotoriges Amphibienflugzeug von Dornier Seawings. Das Flugzeug verfügt über Kurzstart- und -landeeigenschaften. In seiner Fluggastversion ist es für zwei Piloten und 12 Passagiere (oder einen Piloten und 9 Passagiere) ausgelegt. Wesentliche Grundprinzipien seiner Konzeption wurden von der bereits in den 1920er Jahren erfolgreichen Dornier-Wal-Baureihe übernommen und fortentwickelt; so findet sich bei dieser Maschine die Hochdeckerbauweise mit zusätzlichen untenliegenden Stummelflügeln ebenso wieder wie die Tandemanordnung der beiden Motoren.",
+
 		},
-		
+		en = {
+
+			["mod_name"] = "Dornier Seastar",
+			["mod_desc"] = "The Dornier Seastar is a twin-engine amphibious aircraft from Dornier Seawings. The aircraft has short take-off and landing capabilities. In its passenger version, it is designed for two pilots and 12 passengers (or one pilot and 9 passengers). Essential basic principles of its design were adopted and further developed from the Dornier Whale series, which had already been successful in the 1920s; for example, the high-wing design with additional stub wings below and the tandem arrangement of the two engines can be found in this aircraft.",
+
+			["seastar_menu"] = "Dornier Seastar",
+			["seastar_1_name"] = "Dornier Seastar",
+            ["Seastar Asset Set"] = "Seastar Asset Set",
+            ["Statische Seastar Flugzeuge."] = "Static Seastar aircrafts.",
+
+            ["seastar_desc"] = "The Dornier Seastar is a twin-engine amphibious aircraft from Dornier Seawings. The aircraft has short take-off and landing capabilities. In its passenger version, it is designed for two pilots and 12 passengers (or one pilot and 9 passengers). Essential basic principles of its design were adopted and further developed from the Dornier Whale series, which had already been successful in the 1920s; for example, the high-wing design with additional stub wings below and the tandem arrangement of the two engines can be found in this aircraft.",
+
+		},
+
 	}
 end
